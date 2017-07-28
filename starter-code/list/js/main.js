a4acd47bebf49e110998f356269102012b0b4405
Zell Liew <zellwk@gmail.com>
2017-07-28 22:41:50 +0800

    add list
    Signed-off-by: Zell Liew <zellwk@gmail.com>

---
+++ starter-code/list/js/main.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('form')
+const list = document.querySelector('.list')
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const todo = this.elements.todo.value
+
+  if (!todo) return
+
+  const listItem = document.createElement('li')
+  listItem.innerHTML = todo
+  list.append(listItem)
+
+  this.reset()
+})
+
+list.addEventListener('click', function (e) {
+  const listItem = e.target
+  list.removeChild(listItem)
+})
