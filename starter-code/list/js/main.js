ba07681121753006b40fb34e86c39a4890cfc12d
Zell Liew <zellwk@gmail.com>
2017-07-29 10:06:46 +0800

    remove code
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- starter-code/list/js/main.js
+++ starter-code/list/js/main.js
@@ -1,20 +1,55 @@
-const form = document.querySelector('form')
-const list = document.querySelector('.list')
+// What do we have to do to get type something and have it show up in the list
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault()
-  const todo = this.elements.todo.value
 
-  if (!todo) return
 
-  const listItem = document.createElement('li')
-  listItem.innerHTML = todo
-  list.append(listItem)
 
-  this.reset()
-})
 
-list.addEventListener('click', function (e) {
-  const listItem = e.target
-  list.removeChild(listItem)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector('form')
+// const list = document.querySelector('.list')
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault()
+//   const todo = this.elements.todo.value
+
+//   if (!todo) return
+
+//   const listItem = document.createElement('li')
+//   listItem.innerHTML = todo
+//   list.append(listItem)
+
+//   this.reset()
+// })
+
+// list.addEventListener('click', function (e) {
+//   const listItem = e.target
+//   list.removeChild(listItem)
+// })
